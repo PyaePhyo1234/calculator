aecd8efb6aa1131fca631de136e478a4c4218c93
PyaePhyo1234 <pphein01@gmail.com>
2024-05-05 16:08:01 +0630

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,42 @@
+import "./App.css";
+import Button from "./components/Button";
+import Input from "./components/Input";
+import Output from "./components/Output";
+import { useState } from "react";
+
+function App() {
+  const [output, setOutputValue] = useState(0);
+  const [input, setInputValue] = useState(0);
+
+  function clickEventHandling(value) {
+    if (value == "AC") {
+      setInputValue(0);
+      setOutputValue(0);
+    } else if (value == "C") {
+     if(input != 0){
+      input.length == 1
+      ? setInputValue(0)
+      : setInputValue(input.slice(0, input.length - 1));
+     }
+    } else if (value == "=") {
+      setOutputValue(eval(input));
+      setInputValue(0);
+    } else {
+      input == 0 ? setInputValue(value) : setInputValue(input + value);
+     
+    }
+  }
+  return (
+    <>
+      <div className="container">
+        <div className="calculator_frame">
+          <Output output={output} />
+          <Input input={input} />
+          <Button clickEventHandling={clickEventHandling} />
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Input.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import "./Input.css"
+
+const Input = ({input}) => {
+  return (
+    <div className='inputTxt'>{input}</div>
+  )
+}
+
+export default Input
